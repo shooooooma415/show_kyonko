ae301e64237936a5608030909ea9e9744eadbc17
Shoma <shoma@nakanoshoubanoMacBook-Air.local>
2024-06-12 20:08:00 +0900

    写真を対応させた

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Request,BackgroundTasks, Header
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage
+from linebot.models import MessageEvent, TextMessage, ImageSendMessage
 from starlette.exceptions import HTTPException
 import os
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -33,9 +34,28 @@ async def callback(
 
     return "ok"
 
+
 @handler.add(MessageEvent)
 def handle_message(event):
     if event.type != "message" or event.message.type != "text":
         return
-    message = TextMessage(text=event.message.text + "hallo")
-    line_bot_api.reply_message(event.reply_token, message)
+    
+    message_text = event.message.text.lower()
+    
+    if "写真" in message_text:
+        send_random_photo(event.reply_token)
+    else:
+        message = TextMessage(text=event.message.text + " hello")
+        line_bot_api.reply_message(event.reply_token, message)
+
+def send_random_photo(reply_token):
+    photo_directory = "images1"  # 写真が格納されているディレクトリ
+    photos = os.listdir(photo_directory)
+    photo_path = os.path.join(photo_directory, random.choice(photos))
+
+    # 画像を送信する
+    message = ImageSendMessage(
+        original_content_url=photo_path,
+        preview_image_url=photo_path
+    )
+    line_bot_api.reply_message(reply_token, message)
